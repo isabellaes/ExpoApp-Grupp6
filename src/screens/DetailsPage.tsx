5ec0ef48e0d042aab12a3ece2c7993b144362a4f
Eva-Li8E <evaliboden@gmail.com>
2022-09-23 13:22:12 +0200

    signUpPage

--- src/screens/DetailsPage.tsx
+++ src/screens/DetailsPage.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { View } from "react-native";
 
 export default function DetailsPage() {
-  return <View></View>;
+  return <View>DetailsPage</View>;
 }
--- src/screens/ReceptPage.tsx
+++ src/screens/ReceptPage.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { View } from "react-native";
 
 export default function ReceptPage() {
-  return <View></View>;
+  return <View>ReceptPage</View>;
 }
