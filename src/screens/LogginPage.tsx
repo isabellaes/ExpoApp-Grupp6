cf6b1c90dac6170f0acffbcf9d717ee232f04505
Annangggg <dnnguyen000@gmail.com>
2022-09-25 18:42:56 +0200

    added button

--- src/screens/LogginPage.tsx
+++ src/screens/LogginPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, View } from "react-native";
+import { Button, View, Text } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../App";
 
@@ -8,7 +8,7 @@ type Props = NativeStackScreenProps<RootStackParamList, "LoggIn">;
 export default function LoggInPage({ navigation, route }: Props) {
   return (
     <View>
-      LoggInPage
+      <Text>LoggInPage</Text> 
       <Button
         title="Off line mood"
         onPress={() => navigation.navigate("Recept")}
