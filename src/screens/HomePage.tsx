c5541f16bf34a2c0f8ee4769e88b54de7a4d921b
Eva-Li8E <evaliboden@gmail.com>
2022-09-25 15:17:48 +0200

    set up navigation

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,6 +1,26 @@
 import React from "react";
-import { View } from "react-native";
+import { Button, View, Text } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../App";
 
-export default function HomePage() {
-  return <View></View>;
+type Props = NativeStackScreenProps<RootStackParamList, "Home">;
+
+export default function HomePage({ navigation, route }: Props) {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button
+        title="Gå till detaljvyn"
+        onPress={() => navigation.navigate("Details")}
+      />
+      <Button
+        title="Go to Sign In"
+        onPress={() => navigation.navigate("LoggIn")}
+      />
+      <Button
+        title="Off line mood"
+        onPress={() => navigation.navigate("Recept")}
+      />
+    </View>
+  );
 }
