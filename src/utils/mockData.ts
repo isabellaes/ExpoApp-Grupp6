54ef8f5a02409a6a4b25e382cd13fd6e9e1cff00
Eva-Li8E <evaliboden@gmail.com>
2022-09-25 16:02:15 +0200

    mockdata test 2 stycken

--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -7,8 +7,21 @@ const data: Recept[] = [
     receptImage:
       "https://www.matratt.se/wp-content/uploads/2021/07/DSC09555-2-1536x1536.jpg",
     receptName: "Korvstroganoff",
-    receptDescription: "jskljfldk",
-    receptIngridients: "dfjsjgdklks",
+    receptDescription:
+      "Sätt en kastrull med vatten på kokning, och ha därefter i ris som blir bäst efter ca 11 min. Strimla under tiden korven i önskad storlek och stek i olja ihop med hackad gullök.När detta börjar få färg så tillsätts övriga ingredienser, låt sedan stå och små puttra några minuter. Önskas en lättare konsistens på såsen så går alltid lite extra vatten bra att tillsätta, tänk bara på att det isåfall även kan behövas lite mer kryddor. Servera med det färdigkokta riset",
+    receptIngridients:
+      "3/4 dl (60g) Basmatiris, 3 st Naturell korv eller 150g matkorv Mat Rätt, 1 1/2 dl Milda mat lätt 4%, 2/3 dl Vatten, 2 msk Chilisås osötad Felix, 2 tsk Tomatpuré, 2 tsk Japansk soja, 1/2 dl Gul lök, 2/3 tsk Olja, 1 krm Svartpeppar, 1/3 krm Vitpeppar, 1 krm Salt",
+  },
+  {
+    id: 2,
+    protein: "meat",
+    receptImage:
+      "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg",
+    receptName: "Lax med grönsallad",
+    receptDescription:
+      "1. Sallad: Tvätta och skär potatisen i klyftor. Koka potatisklyftorna i lite saltat vatten ca 10 min, eller tills de känns färdigkokta. Klar 2. Koka äggen så att gulan fortfarande är lite mjuk, ca 7- 8 min från de att vattnet har börjat koka. Detta varierar lite beroende på spis. Spola dem sedan kalla, skala och skär dem i skivor. Klar 3. Skölj och halvera tomaterna och skölj och skär gurkan i bitar. Skölj salladen. Klar 4. Dela varje laxfilé i 4 bitar så att du får stora kuber. Salta och peppra på båda sidor. Stek laxkuberna i olja i en varm stekpanna ca 3-4 min på båda sidor. Fortsättning efter annonsen Klar 5. Dressing: Vispa ihop olja, balsamvinäger, senap och honung i en skål. Krydda med en nypa salt och peppar. Kla  6. Montering: Blanda samman kokt potatis, sallad, tomat, oliver och gurka med dressingen. Lägg upp på ett fat och toppa med lax och ägg.",
+    receptIngridients:
+      "Dressing 1/2 dl, olivolja 2 msk, balsamvinäger, vit 2 msk, dijonsenap, 1 tsk, honung, salt, svartpeppar, Sallad 600 g,potatis(ar) 2 st, ägg, 500 g laxfilé, tinad, olivolja, 1/2 ask cocktailtomat(er), 1/2 stgurka, 1 asksalladsmix, 1 burkoliversaltsvartpeppar",
   },
 ];
 
