845e1daff2cdc64b97d53fa867504c8766c168d0
Isabella Ekdahl <isabellaekdahl@outlook.com>
2022-09-26 15:41:46 +0200

    log in form component

--- src/screens/LogginPage.tsx
+++ src/screens/LogginPage.tsx
@@ -1,15 +1,10 @@
-import { Button, View, Text, TextInput } from "react-native";
+import { Button, View, Text, TextInput, StyleSheet } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../App";
-import {
-  Formik,
-  FormikHelpers,
-  FormikProps,
-  Form,
-  Field,
-  FieldProps,
-} from "formik";
+import * as yup from "yup";
+import { Formik } from "formik";
 import React from "react";
+import { LogInForm } from "../Components/LogInForm";
 
 type Props = NativeStackScreenProps<RootStackParamList, "LoggIn">;
 
@@ -18,43 +13,23 @@ interface Values {
   password: string;
 }
 
-function handleFormSubmit(values) {
-  navigation.navigate("Recept");
-}
 export default function LoggInPage({ navigation, route }: Props) {
+  function handleFormSubmit() {
+    navigation.navigate("Recept");
+  }
+
   return (
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={(values) => console.log(values)}
-    >
-      {({ handleChange, handleBlur, handleSubmit, values }) => (
-        <View>
-          <TextInput
-            onChangeText={handleChange("email")}
-            onBlur={handleBlur("email")}
-            value={values.email}
-          />
-          <TextInput
-            onChangeText={handleChange("password")}
-            onBlur={handleBlur("password")}
-            value={values.password}
-          />
-          <Button onPress={handleSubmit} title="Submit" />
-
-          <View>
-            <Text>LoggInPage</Text>
-
-            <Button
-              title="Off line mood"
-              onPress={() => navigation.navigate("Recept")}
-            />
-            <Button
-              title="Sign Up"
-              onPress={() => navigation.navigate("SignUp")}
-            />
-          </View>
-        </View>
-      )}
-    </Formik>
+    <View>
+      <Text>LoggInPage</Text>
+      <LogInForm></LogInForm>
+      <Button
+        title="Offline mood"
+        onPress={() => navigation.navigate("Recept")}
+      />
+      <Button
+        title="Dont have an account yet? Sign up here!"
+        onPress={() => navigation.navigate("SignUp")}
+      />
+    </View>
   );
 }
