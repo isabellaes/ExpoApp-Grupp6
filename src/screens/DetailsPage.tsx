ef576f368827d9af9865e145903a27d69f164901
Eva-Li8E <evaliboden@gmail.com>
2022-09-26 11:37:12 +0200

    created a card comonent

--- src/screens/DetailsPage.tsx
+++ src/screens/DetailsPage.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import { View, Image, StyleSheet, Text, Button } from "react-native";
 import { RootStackParamList } from "../../App";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import Card from "../Components/Card";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Details">;
 
 export default function DetailsPage({ navigation, route }: Props) {
   return (
     <View>
-      <Text>Hej</Text>
-      <Image
-        style={styles.tinyLogo}
-        source={{
-          uri: "https://www.matratt.se/wp-content/uploads/2021/07/DSC09555-2-1536x1536.jpg",
-        }}
-      />
+      <Card>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: "https://www.matratt.se/wp-content/uploads/2021/07/DSC09555-2-1536x1536.jpg",
+          }}
+        />
+        <Text>
+          Hej
+          jaskjfkdjkfjaksjkjkfdjsakljfkljfkjfkljsdakjfkljfkjsdaklfjkladjfkjsdakljfkadjfkljkfldjklfjkljfkljkljklajkldjflkjaflkjflkdsj
+        </Text>
+      </Card>
+
       <Button title="Recept" onPress={() => navigation.navigate("Recept")} />
       <Button title="Home" onPress={() => navigation.navigate("Home")} />
     </View>
