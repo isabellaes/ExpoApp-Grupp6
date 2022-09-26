a6a67c74f2cf31a4a46b187a12bde98d37e1c0f0
Eva-Li8E <evaliboden@gmail.com>
2022-09-26 12:35:32 +0200

    created cardcontent in detailpage

--- src/screens/ReceptPage.tsx
+++ src/screens/ReceptPage.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { Button, View, Text, StyleSheet, ScrollView, Image  } from "react-native";
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+} from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../App";
 import { data } from "../utils/mockData";
-
 
 type Props = NativeStackScreenProps<RootStackParamList, "Recept">;
 
@@ -12,58 +18,62 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   item: {
-    width: '80%',
-    height: 'auto',
+    width: "80%",
+    height: "auto",
     padding: 20,
-    borderRadius: 12, 
+    borderRadius: 12,
     margin: 5,
-    backgroundColor: '#A1A6AA',
-  }, 
+    backgroundColor: "#A1A6AA",
+  },
   title: {
     fontSize: 20,
-    color: 'black',
+    color: "black",
     margin: 5,
   },
   protein: {
     fontSize: 18,
-    color: 'black',
+    color: "black",
     margin: 5,
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 165,
   },
   description: {
-    color: 'black',
+    color: "black",
     margin: 5,
   },
   homeAndLogInButton: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     height: 50,
-    backgroundColor: '#B0C2D4'
-  }
+    backgroundColor: "#B0C2D4",
+  },
 });
 
 export default function ReceptPage({ navigation, route }: Props) {
-
   return (
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={{alignItems: 'center'}}>
+      <ScrollView contentContainerStyle={{ alignItems: "center" }}>
         {data.map((item) => (
           <View style={styles.item}>
-            <Image style={styles.image} source={{uri: item.receptImage}}/>
+            <Image style={styles.image} source={{ uri: item.receptImage }} />
             <Text style={styles.title}>{item.receptName}</Text>
             <Text style={styles.protein}>Protein: {item.protein}</Text>
-            <Text numberOfLines={1} style={styles.description}>{item.receptDescription}</Text>
-            <Button title="Details" onPress={() => navigation.navigate("Details")} />
+            <Text numberOfLines={1} style={styles.description}>
+              {item.receptDescription}
+            </Text>
+            <Button
+              title="Details"
+              onPress={() => navigation.navigate("Details")}
+            />
           </View>
         ))}
-      </ScrollView> 
+      </ScrollView>
       <View style={styles.homeAndLogInButton}>
         <Button title="Home" onPress={() => navigation.navigate("Home")} />
         <Button title="Logg In" onPress={() => navigation.navigate("LoggIn")} />
       </View>
-  </View>
-  )
+    </View>
+  );
 }
