83f5ea8f0e4861e4180af8d7a05845b93f9bd032
Annangggg <dnnguyen000@gmail.com>
2022-09-30 09:04:33 +0200

    add splash-screen

--- src/Components/listRecept.tsx
+++ src/Components/listRecept.tsx
@@ -23,12 +23,14 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    width: "100%",
+    height: "100%"
   },
 });
 
 const Item: FC<Recept> = (props) => {
   return (
-    <View style={styles.container}>
+    <View>
       <Image style={styles.image} source={{ uri: props.receptImage }} />
       <Text style={styles.title}>{props.receptName}</Text>
       <Text style={styles.protein}>{props.protein}</Text>
